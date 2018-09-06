09dcd03be128cc883eab9e472a8be999c7afd017
nimbus1212 <swarnim.1212@gmail.com>
2018-09-07 01:42:58 +0530

    animation fix

--- public/js/main.js
+++ public/js/main.js
@@ -4,5 +4,10 @@ $(document).ready(function() {
       setTimeout(function() {
           $('body').addClass('loaded');
       }, 1000);
-   
+
+      $("#ul").hide();
+
+    // Show the div in 5s
+    $("#ul").delay(3000).fadeIn(500);
   });
+  
