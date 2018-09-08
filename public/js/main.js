fe6beb42f63436015c032e4ef07fb68cd501d831
nimbus1212 <swarnim.1212@gmail.com>
2018-09-08 18:47:59 +0530

    fixed bug

--- public/js/main.js
+++ public/js/main.js
@@ -10,12 +10,3 @@ $(document).ready(function () {
   // Show the div in 3s
   $("#ul").delay(3000).fadeIn(500);
 });
-
-$(document).ready(function () {
-  function reorient(e) {
-    var portrait = (window.orientation % 180 == 0);
-    $("body > div").css("-webkit-transform", !portrait ? "rotate(-90deg)" : "");
-  }
-  window.onorientationchange = reorient;
-  window.setTimeout(reorient, 0);
-});
