10e7fa16ba80a5e304096471c5a9e16a885d9c04
nimbus1212 <swarnim.1212@gmail.com>
2018-09-20 03:43:49 +0530

    favicon

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.listen(process.env.PORT || 3000);
 
 app.use('*/js', express.static(path.join(__dirname, 'public/js')));
 app.use('*/css', express.static(path.join(__dirname, 'public/css')));
+app.use('*/media', express.static(path.join(__dirname, 'public/media')));
 app.use(bodyParser.json());
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(bodyParser.urlencoded({ extended: false }));
