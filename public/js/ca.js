be827f28ae108a02c210be1e9a4c537b34415320
nimbus1212 <swarnim.1212@gmail.com>
2018-11-03 13:32:17 +0530

    ca style

---
+++ public/js/ca.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('.nav__toggle').on('click', function() {
+      $('.nav').toggleClass('display');
+    });
+    $('.nav__toggle').on('click', function() {
+      $('.menu__item').toggleClass('display');
+    });
+  });
